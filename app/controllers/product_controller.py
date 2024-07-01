0d1448a2e371d4b29452ffee8f1c675b57de1f45
Franco Roldan <fr.roldan@alumno.um.edu.ar>
2024-07-01 17:39:54 -0300

    arreglos menores

--- app/controllers/product_controller.py
+++ app/controllers/product_controller.py
@@ -7,7 +7,7 @@ product_schema_many = ProductSchema(many=True) # es para que devuelva varios obj
 product_schema = ProductSchema()
 product = Blueprint('product', __name__)
 
-@product.route('/product/', methods=['GET'])
+@product.route('/all/', methods=['GET'])
 def index():
     list = service.find_all()
     result = product_schema_many.dump(list)
@@ -15,14 +15,14 @@ def index():
     resp.status_code = 200
     return resp
 
-@product.route('/product/id/<int:id>', methods=['GET'])
+@product.route('/id/<int:id>', methods=['GET'])
 def find_by_id(id):
     response = product_schema.dump(service.find_by_id(id))
     resp = jsonify(response)
     resp.status_code = 200
     return resp
 
-@product.route('/product/name/', methods=['GET'])
+@product.route('/name/', methods=['GET'])
 def find_by_name():
     name = request.args.get('name')
 
@@ -31,35 +31,35 @@ def find_by_name():
     resp.status_code = 200
     return resp
 
-@product.route('/product/caliber/<string:caliber>', methods=['GET'])
+@product.route('/caliber/<string:caliber>', methods=['GET'])
 def find_by_caliber(caliber):
     response = product_schema.dump(service.find_by_caliber(caliber))
     resp = jsonify(response)
     resp.status_code = 200
     return resp
 
-@product.route('/product/brand/<string:brand>', methods=['GET'])
+@product.route('/brand/<string:brand>', methods=['GET'])
 def find_by_brand(brand):
     response = product_schema.dump(service.find_by_brand(brand))
     resp = jsonify(response)
     resp.status_code = 200
     return resp
 
-@product.route('/product/type/<string:type>', methods=['GET'])
+@product.route('/type/<string:type>', methods=['GET'])
 def find_by_type(type):
     response = product_schema.dump(service.find_by_type(type))
     resp = jsonify(response)
     resp.status_code = 200
     return resp
 
-@product.route('/product/serial_number/<string:serial_number>', methods=['GET'])
+@product.route('/serial_number/<string:serial_number>', methods=['GET'])
 def find_by_serial_number(serial_number):
     response = product_schema.dump(service.find_by_serial_number(serial_number))
     resp = jsonify(response)
     resp.status_code = 200
     return resp
 
-@product.route('/product/create/', methods=['POST'])
+@product.route('/create/', methods=['POST'])
 def create_product():
     product = product_schema.load(request.json)
     response = product_schema.dump(service.create(product))
@@ -67,7 +67,7 @@ def create_product():
     resp.status_code = 200
     return resp
 
-@product.route('/product/update/<int:id>', methods=['PUT'])
+@product.route('/update/<int:id>', methods=['PUT'])
 def update_product(id):
     product = request.json
     response = product_schema.dump(service.update(product, id))
@@ -75,7 +75,7 @@ def update_product(id):
     resp.status_code = 200
     return resp
 
-@product.route('/product/delete/<int:id>', methods=['DELETE'])
+@product.route('/delete/<int:id>', methods=['DELETE'])
 def delete_product(id):
     response = product_schema.dump(service.delete(id))
     resp = jsonify(response)
